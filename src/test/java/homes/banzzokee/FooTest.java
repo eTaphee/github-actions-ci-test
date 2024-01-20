b3ed60492993702d25f5c6809ee25b0a7ff7e1be
thlee <e-tap@naver.com>
2024-01-20 22:31:45 +0900

    Create FooTest.java

---
+++ src/test/java/homes/banzzokee/FooTest.java
@@ -0,0 +1,18 @@
+package homes.banzzokee;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+
+    @Test
+    void someTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void someTest2() {
+        assertEquals(1, 2);
+    }
+}
