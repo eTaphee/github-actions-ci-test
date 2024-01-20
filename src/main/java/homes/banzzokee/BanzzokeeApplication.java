40c89f034277230afc403a662d42a19ebb6f7576
thlee <e-tap@naver.com>
2024-01-20 21:35:22 +0900

    init

---
+++ src/main/java/homes/banzzokee/BanzzokeeApplication.java
@@ -0,0 +1,13 @@
+package homes.banzzokee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanzzokeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanzzokeeApplication.class, args);
+	}
+
+}
