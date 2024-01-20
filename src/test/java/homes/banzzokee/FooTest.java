ecdb8bd64aa10caf952544e7049f7c597f23b7c5
thlee <e-tap@naver.com>
2024-01-20 23:07:43 +0900

    Update FooTest.java

--- src/test/java/homes/banzzokee/FooTest.java
+++ src/test/java/homes/banzzokee/FooTest.java
@@ -16,7 +16,7 @@ public class FooTest {
     @DisplayName("This is a test with a display name")
     void someTest2() {
         assertEquals(2, 1);
-        assertEquals(2, 2);
+        assertEquals(2, 3);
         assertEquals(2, 2);
         assertEquals(2, 2);
     }
